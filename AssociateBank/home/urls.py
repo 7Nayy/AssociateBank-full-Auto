3d0af97a4e83a905be0a6711fb0c2330f70eea10
7Nayy <nadim.benkrouidem@gmail.com>
2023-12-14 17:25:01 +0100

    add password changed

--- AssociateBank/home/urls.py
+++ AssociateBank/home/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
@@ -5,5 +6,10 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='')
+    path('logout/', auth_views.LogoutView.as_view(), name=''),
+
+    #password change
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
 ]
