b4432449e848fecaad8b49b9e51cc80b9e7c96c7
7Nayy <nadim.benkrouidem@gmail.com>
2023-12-15 00:17:08 +0100

    add sign up

--- AssociateBank/home/views.py
+++ AssociateBank/home/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .forms import *
 
 
 @login_required
 def home(request):
     return render(request, 'registration/home.html',{'section' : 'home'})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            # create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # set the choosen pwd
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            # Save the user object
+            new_user.save()
+            return render(request, 'registration/register_done.html',{'new_user' : new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'registration/register.html',{'user_form' : user_form})
